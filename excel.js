2be919d4712a0a8b09a35b2c8ef66039f162be53
yeyan1996 <1366792281@qq.com>
2018-12-14 15:49:56 +0800

    test:sourcetree测试

--- excel.js
+++ excel.js
@@ -28,14 +28,45 @@ function sliceByColumn(options) {
     return slicedObject
 }
 
+function replaceFormItem(initCode, excelObj) {
+    let arr = initCode.match(/<el-form-item[^>]*>[\s\S]*?<\/el-form-item>/g);//arr匹配到的是标签的前半部分
+    // console.log(arr)
+    let sourceArr = JSON.parse(JSON.stringify(arr));// 深拷贝arr
+    let index = 0;//储存上一次的end位置
+
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].replace(/v-model="[^"]*"/g,`v-model="${excelObj.prop[i]}"`)
+        arr[i] = arr[i].replace(/label="([\u4e00-\u9fa5]|\w)+"/g, function(match,index){
+            if(index === '0') return `label="${colObj.label[i]}"`
+            return match
+        })
+    }
+
+    //从各个匹配的位置替换原来标签
+    for (let i = 0; i < sourceArr.length; i++) {
+        let start=initCode.indexOf(sourceArr[i],index);
+        let len=sourceArr[i].length
+        let end=start+len;
+        initCode=initCode.substring(0,start-1)+arr[i]+initCode.substring(end,initCode.length);
+        index=end;
+    }
+
+    console.log(initCode)
+    return initCode
+}
+
 
 function replaceCode(initCode, colObj) {
-    console.log('截取列表', colObj)
     let arr = initCode.match(/([\\s\S]*?)(<el-table[^>]*>)/g);//arr匹配到的是标签的前半部分
     let sourceArr = JSON.parse(JSON.stringify(arr));// 深拷贝arr
     let str = ""
     for (let i = 0; i < colObj.label.length; i++) {
-        str += `\n <el-table-column width="160" prop="${colObj.prop[i]}" label="${colObj.label[i]}"></el-table-column> \n`
+        str += `\n 
+                <el-table-column
+                 width="160"
+                 prop="${colObj.prop[i]}" 
+                 label="${colObj.label[i]}">
+                </el-table-column>`
     }
     ////从各个匹配的位置替换原来标签
     let openTagStart = initCode.indexOf(sourceArr[0]);
@@ -57,6 +88,7 @@ async function writeFile(writeData) {
 }
 
 let colObj = sliceByColumn(config.options)
+console.log(colObj)
 
 readFile()
     .then(readData => {
@@ -64,5 +96,3 @@ readFile()
         writeFile(replacedCode)
     })
     .catch(err => console.log(err))
-
-
