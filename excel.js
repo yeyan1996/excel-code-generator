f942a4b128f61163fc792100b53f9312ca8fd70e
yeyan1996 <1366792281@qq.com>
2018-12-15 10:46:59 +0800

    feat:添加npm script style:重命名模块

--- excel.js
+++ excel.js
@@ -4,9 +4,9 @@ const xlsx = require("node-xlsx")
 const config = require('./config')
 const sliceByColumn = require('./sliceByColumn')
 const replaceFormItem = require('./replaceFormItem')
-const generatorTableColumn = require('./generatorTableColumn')
-const generatorColumnsData = require('./generatorColumnsData')
-
+const generatorTableColumn = require('./generateTableColumn')
+const generatorColumnsData = require('./generateColumnsData')
+console.log(process.argv[2])
 let workSheetsFromBuffer = xlsx.parse(fs.readFileSync(config.path));
 //第几张sheet
 let {data} = workSheetsFromBuffer[config.sheet - 1];
@@ -26,12 +26,22 @@ let colObj = sliceByColumn(config.options, data)
 console.log(colObj)
 
 
-
-
 readFile()
     .then(readData => {
-        // let replacedCode = generatorTableColumn(readData, colObj)
-        let replacedCode = generatorColumnsData(readData, colObj)
+        let replacedCode;
+        switch (process.argv[2]) {
+            case 'generateTableColumn':
+                replacedCode = generatorTableColumn(readData, colObj)
+                break;
+            case 'generateColumnsData':
+                replacedCode = generatorColumnsData(readData, colObj)
+                break;
+            case 'replaceFormItem':
+                replacedCode = replaceFormItem(readData, colObj)
+                break;
+            default:
+                replacedCode = generatorColumnsData(readData, colObj)
+        }
         writeFile(replacedCode)
     })
     .catch(err => console.log(err))
