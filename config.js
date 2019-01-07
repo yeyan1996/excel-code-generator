4d3b7f17f11236f08248237c2a41c05938e4c0c0
yeyan1996 <1366792281@qq.com>
2019-01-07 17:58:47 +0800

    doc:添加必要注释

--- config.js
+++ config.js
@@ -1,18 +1,22 @@
 module.exports = {
     path:'C:/Users/007/Desktop/excel.xlsm',
     // path: 'C:/Users/zhl/Desktop/excel.xlsx',
-    sheet: 7,
+    sheet: 6,
     options: [
         {
             name: "prop",
-            line: [3, 4, 15],
+            //第1个参数为第x列
+            //第2个参数为第x行
+            //第3个参数为提取到x列(以excel的顶部为第一列)
+            line: [3, 4, 56],
+            // 是否需要转成驼峰
             cameCase: true
         },
         {
             name: "label",
-            line: [4, 4, 15],
+            line: [4, 4, 56],
         }
     ],
-    readFilePath: 'E:/HospitalManagement/dp2plus/columns.ts',
-    writeFilePath: 'E:/HospitalManagement/dp2plus/columns.ts'
+    readFilePath: 'E:/HospitalManagement/dp2plus/src/views/hospCatalogSearch/detailColumns.ts',
+    writeFilePath: 'E:/HospitalManagement/dp2plus/src/views/hospCatalogSearch/detailColumns.ts'
 }
