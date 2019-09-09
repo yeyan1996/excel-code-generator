57b39944e6aad0b5f666483b07e5807d46981cad
周浩磊 <1996yeyan@gmail.com>
2019-09-09 16:43:49 +0800

    feat:将 ts 编译的 js 文件都放入 dist 目录，并修改 npm script 调整 ts 的 debugger 命令

--- src/util/camelCase.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function camelCase(str) {
-    return str.replace(/_(\w)/g, function (match) {
-        return match.slice(1, 2).toUpperCase();
-    });
-}
-exports.camelCase = camelCase;
-//# sourceMappingURL=camelCase.js.map
--- src/util/warn.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function warn(errMessage) {
-    console.error(errMessage);
-    process.exit();
-}
-exports.warn = warn;
-//# sourceMappingURL=warn.js.map
