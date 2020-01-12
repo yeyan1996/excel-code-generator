e6a2aedfc5855aeb5e40495790dc06158e91180c
zhouhaolei <1366792281@qq.com>
2020-01-12 15:12:47 +0800

    chore: 修改工作流

--- dist/util/camelCase.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function camelcase(str) {
-    return str.replace(/_(\w)/g, function (match) {
-        return match.slice(1, 2).toUpperCase();
-    });
-}
-exports.camelcase = camelcase;
--- script/release.sh
+++ script/release.sh
@@ -3,16 +3,24 @@
 set -e
 echo "Enter release version: "
 read VERSION
-
+echo "Enter git comment: "
+read COMMENT
 read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-
   echo "Releasing $VERSION ..."
 
   # build
   npm run build
+
+  # commit
+  git add -A
+  git commit -m "$COMMENT"
   npm version ${VERSION} --message "[release] $VERSION"
+
+
+  # publish
+  git push
   npm publish
 fi
